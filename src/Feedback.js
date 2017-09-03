c13b401742a93a79094cb41ee7cbea6823973948
guym <guym@wix.com>
2017-09-04 01:09:00 +0300

    dev feedback

--- src/Feedback.js
+++ src/Feedback.js
@@ -3,24 +3,189 @@
  */
 import React, {Component} from "react";
 import {View, Animated, PanResponder, Dimensions, LayoutAnimation, UIManager, Text} from "react-native";
+import TimerMixin from "react-timer-mixin";
+let reactMixin = require('react-mixin');
+
+import Choice from "./Choice";
 
 export default class Feedback extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 0
+        };
+        this.widthAnim = new Animated.Value(230);
+        this.textsOnCorrect = [
+            "Correct Answer!",
+            "Great Answer!",
+            "Awesome Answer!",
+            "Gorgeous Answer!",
+            "Spectacular Answer",
+        ];
+        this.textsOnIncorrect = [
+            "Wrong!",
+            "Ouch!",
+            "Noooooo!",
+            "OMG You Suck!",
+            "Just Give Up Already...",
+        ];
+        let results = this.check(props.expectedAnswer, props.actualAnswer);
+        let entries = [];
+        let totalScore = 0;
+        let combo = 1;
+        let scorePerChoice = 0;
+        let elements = [];
+        for (let i = 0; i < results.length; i++) {
+            let result = results[i];
+            let isLast = i === results.length - 1;
+            scorePerChoice = result ? 100 * combo : 0;
+            entries.push({
+                combo: combo,
+                score: scorePerChoice,
+                result: result,
+                text: !isLast ? "+" + scorePerChoice : (result ? this.textsOnCorrect[combo] : this.textsOnIncorrect[combo]),
+                isLast: i === results.length - 1
+            });
+            combo = result ? combo + 1 : 1;
+            totalScore += scorePerChoice;
+
+            elements.push(this.getElement(entries[i]));
+        }
+        this.elements = elements;
+        this.score = totalScore;
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.text}>{this.props.text}</Text>
+            <View>
+                {this.elements[this.state.index]}
             </View>
         )
     }
 
     componentDidMount() {
+        console.log("Feedback - componentDidMount");
+        console.log(this.elements);
+        for (let i = 0; i < this.elements.length; i++) {
+            this.setTimeout(() => {
+                const isLast = i === this.elements.length - 1;
+                if (!isLast) {
+                    this.setState({
+                        index: this.state.index + 1
+                    });
+                } else this.onEnd();
+            }, 500 * i);
+        }
+    }
 
+    getElement(entry) {
+        return [
+            <FeedbackCombo style={styles.combo} count={entry.combo}/>,
+            <Choice style={styles.icon} text="" type={entry.result}/>,
+            <FeedbackText style={styles.text} text={entry.text}/>
+        ];
+    }
+
+    // getElement(entry) {
+    //     return [
+    //         <FeedbackCombo style={styles.combo} count={entry.combo}/>,
+    //         <FeedbackIcon style={styles.icon} type={entry.result}/>,
+    //         <FeedbackScore style={styles.score} score={entry.score}/>
+    //     ];
+    // }
+
+    // componentWillReceiveProps(nextProps) {
+    //     console.log("Feedback nextProps " + nextProps);
+    //     if (this.props.id === nextProps.id) return;
+    //     Animated.timing(
+    //         this.state.widthAnim,
+    //         {
+    //             toValue: 50,
+    //             duration: 500,
+    //             easing: Easing.linear,
+    //         }
+    //     ).start();
+    // }
+
+    onEnd() {
+        console.log("Feedback ended");
+        this.setTimeout(() => {
+            this.props.onEnd();
+        }, 500);
+    }
+
+    onChoiceWasEvaluated() {
+        console.log("Choice was evaluated");
+        this.props.onChoiceWasEvaluated({
+            index: this.state.index
+        });
+    }
+
+    check(expected, actual) {
+        let results = [];
+        for (let i = 0; i < expected.length; i++) {
+            results.push(expected === actual);
+        }
+        return results;
+    }
+}
+reactMixin(Feedback.prototype, TimerMixin);
+
+class FeedbackIcon extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 0
+        };
+        // this.widthAnim = new Animated.Value(230);
+    }
+
+    render() {
+        return (
+            <Animated.View style={[styles.icon, {width: this.widthAnim}]}/>
+        )
+    }
+
+    // componentDidMount() {
+    //     Animated.timing(
+    //         this.widthAnim,
+    //         {
+    //             toValue: 50,
+    //             duration: 500,
+    //             // easing: Easing.linear,
+    //         }
+    //     ).start();
+    // }
+}
+
+class FeedbackText extends Component {
+    render() {
+        return (
+            <Animated.View>
+                <Text>{this.props.text}</Text>
+            </Animated.View>
+        )
     }
 }
 
+class FeedbackCombo extends Component {
+    render() {
+        return (
+            <Animated.View>
+                <Text>{this.props.combo}</Text>
+            </Animated.View>
+        )
+    }
+}
+
+class FeedbackScore extends Component {
+
+}
+
 const styles = {
-    container: {
+    icon: {
         width: 230,
         height: 50,
         borderRadius: 50 / 2,
@@ -35,6 +200,11 @@ const styles = {
         shadowOpacity: 0.5
     },
     text: {
-        color: "white"
+        fontSize: 24,
+        color: "#E3E3E3",
+        paddingLeft: 50,
+        backgroundColor: 'rgba(0,0,0,0)',
+        fontFamily: "josefin-sans-bold",
+        textAlign: "left"
     },
 };
